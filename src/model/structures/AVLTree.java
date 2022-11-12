8bee82767c463e392510f2cda200305dc3b2d24e
Luis Dorr <luisdorr@outlook.com>
2022-11-11 23:32:21 -0300

    p

--- src/model/structures/AVLTree.java
+++ src/model/structures/AVLTree.java
@@ -1,185 +1,103 @@
 package model.structures;
 
-public class AVLTree <T extends Comparable<T>>{
+public class AVLTree<T extends Comparable<T>> {
     private Node<T> root;
-    private int height;
 
-    //constructors
     public AVLTree() {
-        this.root = null;
-    }
-    public AVLTree(Node<T> root) {
-        this.root = root;
-    }
-    public AVLTree(int key) {
-        this.root = new Node(key);
     }
 
-    //GETTERS
-    public Node<T> getRoot(){
+    public AVLTree(T key) {
+        this.root = new Node<T>(key);
+    }
+
+    public Node<T> getRoot() {
         return root;
     }
 
-    private int height(Node<T> node) {
-        if (node != null) {
-            return node.getHeight();
-        } else {
-            return 0;
+    //get balance factor of the node
+    public int getHeight(Node<T> root) 
+    {
+        if(root != null) 
+        {
+            return Math.max(getHeight(root.getLeftChild()) + 1, getHeight(root.getRightChild()) + 1);
         }
+        return -1;
     }
-    public void heightUpdate(Node<T> node) {
-        int newHeight = Math.max( height(node.getLeftChild()), height(node.getRightChild()) );
-        node.setHeight(newHeight + 1);
-    }
-
-    public int balance(Node<T> node) {
-        return (root != null) ? height(node.getLeftChild()) - height(node.getRightChild()) : 0;
+    public int getBalanceFactorOf(Node<T> root) 
+    {
+        return getHeight(root.getLeftChild()) - getHeight(root.getRightChild()) ;
     }
 
-    public int getNodeHeight(Node<T> root){
-        if (root == null) {
-            return 0;
+    //Rotations
+    public Node<T> simpleLeftRotation(Node<T> root) 
+    {
+        Node<T> rightNode = root.getRightChild();
+        Node<T> centerNode = rightNode.getLeftChild();
+        rightNode.setLeftChild(root);
+        root.setRightChild(centerNode);
 
-        } else {
-            int leftHeight = getNodeHeight(root.getLeftChild()) + 1;
-            int rightHeight = getNodeHeight(root.getRightChild()) + 1;
-            return Math.max(leftHeight, rightHeight);
-        }
+        return rightNode;
     }
-
-    public int getBalanceFactor(Node<T> node) {
-        return getNodeHeight(node.getLeftChild()) - getNodeHeight(node.getRightChild());
-    }
-
-    public Node<T> treeRebalancing(Node<T> root) {
-        if (getBalanceFactor(root) == 2) {
-            if(getBalanceFactor(root.getLeftChild()) < 0) {
-                doubleRightRotation(root);
-            } else {
-                return simpleRightRotation(root);
-            }
-        } else if (getBalanceFactor(root) == -2) {
-            if(getBalanceFactor(root.getRightChild()) > 0) {
-                return doubleLeftRotation(root);
-            } else {
-                return simpleLeftRotation(root);
-            }
-        }
-
-        return root;
-    }
-
-    public void printTree(Node<T> root, int height) {
-        if (root != null){
-            height++;
-            System.out.println(makeSpaces(height) + root);
-            printTree(root.getLeftChild(),height);
-            printTree(root.getRightChild(), height);
-        }
-    }
-
-    //methods
-
-    public Node<T> add(Node<T> root, T key) {
-        if (root == null) {
-            root = new Node<T>(key);
-            return root;
-
-        } else if (key.compareTo(root.getKey()) < 0) {
-            root.setLeftChild(add(root.getLeftChild(),key));
-        } else if (key.compareTo(root.getKey()) > 0) {
-            root.setRightChild(add(root.getRightChild(),key));
-        } else if (key.compareTo(root.getKey()) == 0 && root != null) {
-            System.out.println( "THIS NUMBER ALREADY EXIST!");
-        }
-
-        return root;
-    }
-
-    public Node<T> remove(Node<T> root, T key) {
-        if (root == null) {
-            System.out.println("This key is not in the tree.");
-            return root;
-
-        } else if (key.compareTo(root.getKey()) < 0) {
-            root.setLeftChild(remove(root.getLeftChild(),key));
-
-        } else if (key.compareTo(root.getKey()) > 0) {
-            root.setRightChild(remove(root.getRightChild(),key));
-
-        } else if (key.compareTo(root.getKey()) == 0) {
-            if(root.getLeftChild() == null || root.getRightChild() == null) {
-                root = (root.getLeftChild() == null) ? root.getRightChild() : root.getLeftChild();
-            } else {
-                T copyKey = getTheBiggestLeftChild(root).getKey();
-                root.setKey(copyKey);
-                
-                root.setLeftChild(remove(root.getLeftChild(), key));
-            }
-        }
-
-        return treeRebalancing(root);
-    }  
-
-    public Node<T> search(Node<T> root, T key) {
-        
-        if (root == null) {
-            System.out.println("This key is not in the tree.");
-            return root;
-        } else if (key.compareTo(root.getKey()) < 0) {
-            return search(root.getLeftChild(),key);
-
-        } else if (key.compareTo(root.getKey()) > 0) {
-            return search(root.getRightChild(),key);
-
-        } else if (key.compareTo(root.getKey()) == 0) {
-            return root;
-        }
-
-        return root;
-    }
-
-    public void inOrder(Node<T> root) {
-		if (root != null) {
-			inOrder(root.getLeftChild());
-			System.out.print(root.getKey() + " ");
-			inOrder(root.getRightChild());
-        }
-	}
-
-    //ROTATIONS
-    public Node<T> simpleRightRotation(Node<T> root) {
+    public Node<T> simpleRightRotation(Node<T> root) 
+    {
         Node<T> newRoot = root.getLeftChild();
-        Node<T> rootsNewLeftChild = newRoot.getRightChild();
+        Node<T> rootNewLeftChild = newRoot.getRightChild();
         newRoot.setRightChild(root);
-        root.setLeftChild(rootsNewLeftChild);
-        heightUpdate(root);
-        heightUpdate(newRoot);
+        root.setLeftChild(rootNewLeftChild);
 
         return newRoot;
     }
-    public Node<T> simpleLeftRotation(Node<T> root) {
-        Node<T> newRoot = root.getRightChild();
-        Node<T> rootsNewRightChild = newRoot.getLeftChild();
-        newRoot.setLeftChild(root);
-        root.setRightChild(rootsNewRightChild);
-        heightUpdate(root);
-        heightUpdate(newRoot);
-
-        return newRoot;
+    public Node<T> doubleLeftRotation(Node<T> root) 
+    {
+        root.setRightChild(simpleRightRotation(root.getRightChild()));
+        return simpleLeftRotation(root);
     }
-    public Node<T> doubleRightRotation(Node<T> root) {
-        simpleLeftRotation(root.getLeftChild());
+    public Node<T> doubleRightRotation(Node<T> root)
+    {
+        root.setLeftChild(simpleLeftRotation(root.getLeftChild()));
         return simpleRightRotation(root);
     }
-    public Node<T> doubleLeftRotation(Node<T> root) {
-        simpleRightRotation(root.getRightChild());
-        return simpleLeftRotation(root);
+
+    public Node<T> balancingTheTree(Node<T> root) 
+    {
+        if (root != null)
+        {
+            if(getBalanceFactorOf(root) == 2) 
+            {
+                return (getBalanceFactorOf(root.getLeftChild()) >= 0 ? simpleRightRotation(root) : doubleRightRotation(root));
+            } 
+            else if(getBalanceFactorOf(root) == -2)
+            {
+                return (getBalanceFactorOf(root.getRightChild()) <= 0 ? simpleLeftRotation(root) : doubleLeftRotation(root));
+            }
+        }
+        return root;
+    }
+
+    public Node<T> add(T key, Node<T> root) 
+    {
+        if (root == null) {
+            root = new Node<>(key);
+            return root;
+        } else if (key.compareTo(root.getKey()) == -1) {
+            root.setLeftChild(add(key, root.getLeftChild()));
+        } else if (key.compareTo(root.getKey()) == 1) {
+            root.setRightChild(add(key, root.getRightChild()));
+        } else {
+            System.out.println("ERROR,  THIS KEY ALREDY EXIST!");
+            return root;
+        } 
+        return balancingTheTree(root);
+    }
+
+    public void add(T key) {
+        this.root = add(key,root);
     }
 
     public Node<T> getTheBiggestLeftChild(Node<T> root){
         return goToTheRightChild(root.getLeftChild());
     }
+
     public Node<T> goToTheRightChild(Node<T> root) {
         if (root.getRightChild() == null) {
             return root;
@@ -188,7 +106,51 @@ public class AVLTree <T extends Comparable<T>>{
         }
     }
 
-    public String makeSpaces(int height) {
+    public Node<T> remove(T key, Node<T> root) 
+    {
+        if (root == null) {
+            System.out.println("This key is not in the tree.");
+            return root;
+
+        } else if (key.compareTo(root.getKey()) < 0) {
+            root.setLeftChild(remove(key, root.getLeftChild()));
+
+        } else if (key.compareTo(root.getKey()) > 0) {
+            root.setRightChild(remove(key, root.getRightChild()));
+
+        } else if (key.compareTo(root.getKey()) == 0) {
+            if(root.getLeftChild() == null || root.getRightChild() == null) {
+                root = (root.getLeftChild() == null) ? root.getRightChild() : root.getLeftChild();
+            } else {
+                T copyKey = getTheBiggestLeftChild(root).getKey();
+                root.setKey(copyKey);
+                
+                root.setLeftChild(remove(copyKey, root.getLeftChild()));
+            }
+        }
+
+        return balancingTheTree(root);
+    }
+
+    public void remove(T key) {
+        this.root = remove(key, root);
+    }
+
+    public boolean search(T key, Node<T> root) 
+    {
+        if (root == null) {
+            return false;
+        } else if (key.compareTo(root.getKey()) == -1) {
+            return search(key, root.getLeftChild());
+        } else if (key.compareTo(root.getKey()) == 1) {
+            return search(key, root.getRightChild());
+        } else {
+            return true;
+        }
+    } 
+
+    public String makeSpaces(int height)
+    {
         String space = "";
         for(int i = 0; i < height; i++) {
             space = space + "    ";
@@ -196,6 +158,22 @@ public class AVLTree <T extends Comparable<T>>{
         return space;
     }
 
+    public void printTree(Node<T> root, int height) 
+    {
+        if (root != null){
+            height++;
+            System.out.println(makeSpaces(height) + root);
+            printTree(root.getLeftChild(),height);
+            printTree(root.getRightChild(), height);
+        }
+    }
 
-
+    public void inOrder(Node<T> root) 
+    {
+		if (root != null) {
+			inOrder(root.getLeftChild());
+			System.out.print(root.getKey() + " ");
+			inOrder(root.getRightChild());
+        }
+	}
 }
